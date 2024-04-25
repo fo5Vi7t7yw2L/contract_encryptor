f572a552ba75949ec26d607e66400737f446f895
fo5Vi7t7yw2L <git@maximin.info>
2024-04-25 16:03:44 +0200

    .

--- src/contract_encryptor/app.py
+++ src/contract_encryptor/app.py
@@ -5,20 +5,55 @@ import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 
+class LBs():
+    
 
 class ProgrammzurVerschlüsselungvonVerträgenvonLehrbeauftragten(toga.App):
     def startup(self):
-        """Construct and show the Toga application.
+        main_box = toga.Box(style=Pack(direction=COLUMN))
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        name_label = toga.Label(
+            "Your name: ",
+            style=Pack(padding=(0, 5))
+        )
+        self.name_input = toga.TextInput(style=Pack(flex=1))
+
+        name_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        name_box.add(name_label)
+        name_box.add(self.name_input)
+
+        button = toga.Button(
+            "Say Hello!",
+            on_press=self.say_hello,
+            style=Pack(padding=5)
+        )
+        button2 = toga.Button(
+            "Georg",
+            on_press=self.say_georg,
+            style=Pack(padding=5)
+        )
+
+        button_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        button_box.add(button)
+        button_box.add(button2)
+        main_box.add(name_box)
+        main_box.add(button_box)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
+
+    def say_hello(self, widget):
+        self.main_window.info_dialog(
+            f"Hello, {self.name_input.value}",
+            "Hi there!"
+        )
+    
+    def say_georg(self, widget):
+        self.main_window.info_dialog(
+            f"Georg",
+            "Hallo, mein Name ist Georg und meine Festplatte ist voll."
+        )
 
 
 def main():
